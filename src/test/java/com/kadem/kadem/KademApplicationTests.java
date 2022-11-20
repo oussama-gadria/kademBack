f3d977c4ef52b4d586aac32655388e7fd8e50560
Oussama Gadria <gadria.oussama@esprit.tn>
2022-11-20 20:52:42 +0100

    Initial commit

---
+++ src/test/java/com/kadem/kadem/KademApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kadem.kadem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KademApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
